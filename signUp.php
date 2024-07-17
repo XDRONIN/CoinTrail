4caef58e52a2baa110f4c0ea52637db89d57eec3
XDRONIN <akashprakashmattathil@gmail.com>
2024-07-17 12:49:20 +0530

    table creation during signup

--- signUp.php
+++ signUp.php
@@ -15,8 +15,34 @@ $name=$_POST["name"];
 $email=$_POST["email"];
 $psswrd=$_POST["psswrd"];
 $sql="INSERT into users (name,email,psswrd) values ('$name','$email','$psswrd')";
+
 $qry=mysqli_query($conn, $sql);
+
 if($qry){
+    $getid="SELECT id FROM users WHERE email='$email'";
+    $getIdQuery=mysqli_query($conn, $getid);
+    $userId=mysqli_fetch_assoc($getIdQuery);
+    $tableId="user_".$userId['id'];
+    //echo $tableId;
+   /* if($userId){
+    echo $userId['id'];}
+    else{
+        echo "error";
+    }*/
+    $insertSql="CREATE TABLE $tableId(
+        id INT(11) NOT NULL AUTO_INCREMENT,
+        COD VARCHAR(20) NOT NULL , 
+        amount INT(11) NOT NULL , 
+        area VARCHAR(50) NOT NULL , 
+        DOT DATE NOT NULL , PRIMARY KEY (id))";//DOT==Date Of Transation COD==Credit Or Debit
+     $insertion=mysqli_query($conn, $insertSql);
+     /*if($insertion){
+        echo "inserted";   }
+        else{
+            echo "not inserted";}*/
+
+
+
     echo "<h1 class='p1'>YOUR REGISTRATION WAS<span class='span1'> SUCCESSFULL</span></h1>";
     echo"<div class='ill'><img class='img' src='signUP.jpg' class='img'></div>";
    echo "<button class='butt' onclick=\"window.location.href='http://localhost/MiniProject/login.php';\">
