349d84d3f2be8a1ef7d987a1e20e6cd0ba3efa79
XDRONIN <akashprakashmattathil@gmail.com>
2024-07-20 22:05:08 +0530

    navbar

--- dashboard.php
+++ dashboard.php
@@ -17,10 +17,30 @@ if (isset($_SESSION["email"])){//getting the email from session
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
 </head>
 <link rel="stylesheet" href="dashboard.css">
 <body>
-    <div class="navbar"></div>
+    <div class="navbar">
+      <div class="menu">
+      <label class="burger" for="burger">
+       <input type="checkbox" id="burger">
+         <span></span>
+         <span></span>
+         <span></span>
+          </label>
+      
+      </div>
+      <h2 class="logo">Coin<span class="span1">Trail</span></h2>
+      <div class="profile">NAME</div>
+    </div>
     <div class="balance">$$</div>
     <div class="container">
       <div class="overview"></div>
