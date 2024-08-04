5f3734006dfd8855d0797d7cdb5c2a1f6dd3bd88
XDRONIN <akashprakashmattathil@gmail.com>
2024-08-04 20:44:55 +0530

    array method

--- test.php
+++ test.php
@@ -1,7 +1,33 @@
 <?php
-$item1=$_POST['item1'];
-$price1=$_POST['price1'];
-$item2=$_POST['item2'];
-$price2=$_POST['price2'];
-echo''.$item1.''.$price1.''.$item2.''.$price2;
+$item0=$_POST['item0'];
+$price0=$_POST['price0'];
+
+//echo''.$item1.''.$price1;
+
+$items=array();
+$items[0]=$item0;
+$prices=array();
+$prices[0]=$price0;
+for($i= 1;$i<50;$i++){
+    $nextItem=$_POST['item'.$i.''];
+    $nextPrice=$_POST['price'.$i.''];
+   // echo''.$nextItem.'';
+   if($nextItem!=null && $nextPrice!=null){
+    $items[$i]=$nextItem;
+    $prices[$i]=$nextPrice;
+   }
+   elseif($nextItem!=null){
+    $items[$i]=$nextItem;
+
+}
+else{
+    break;
+}
+
+}
+foreach($items as $item) {
+    echo"".$item."";}
+ foreach($prices as $price) {
+        echo"".$price."";}    
+
        ?>
