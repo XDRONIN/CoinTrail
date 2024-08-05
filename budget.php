4b35f5c67b0da3225df7fe3cfca5c4116b2c9a77
XDRONIN <akashprakashmattathil@gmail.com>
2024-08-05 19:35:17 +0530

    Budget details fetch

--- budget.php
+++ budget.php
@@ -8,10 +8,7 @@ $pattern = $tbname."_";
 $sql="SELECT table_name FROM information_schema.tables WHERE table_schema = '$db_name' AND table_name LIKE '$pattern%'";
 $query=mysqli_query($conn,$sql);
 //$row=mysqli_fetch_array($query);
-while($row=mysqli_fetch_array($query)){
-    //echo $pattern;
-    echo "".$row["0"];
-}
+
 
 ?>
 <!DOCTYPE html>
@@ -25,8 +22,24 @@ while($row=mysqli_fetch_array($query)){
     <button onclick =" window.location.href='insertBudget.php'">ADD BUDGET</button>
     <div container>
     <?php 
-  
+    while($row=mysqli_fetch_array($query)){
+        //echo $pattern;
+       // echo "".$row["0"];
+       $bTb=$row['0'];
+
+    
     ?>
+    <div class="card">
+    <?php 
+        $cardSql="SELECT * FROM $bTb WHERE id > 1";
+        $cardQuery= mysqli_query($conn,$cardSql);
+        while($cardRow=mysqli_fetch_array($cardQuery)){
+            echo"".$cardRow["1"]." ".$cardRow["2"];
+        
+}?><br></div><?php } ?>
+    
+    
+    
     </div>
 </body>
 </html>
