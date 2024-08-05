b6bbf1a555af927b5dcf53f6fa5329dc6bcd8e40
XDRONIN <akashprakashmattathil@gmail.com>
2024-08-05 18:59:39 +0530

    Getting tablename from DB

--- budget.php
+++ budget.php
@@ -1,3 +1,19 @@
+<?php
+session_start();
+include("connect.php");
+$tbname=$_SESSION['tbname'];
+$pattern = $tbname."_";
+
+
+$sql="SELECT table_name FROM information_schema.tables WHERE table_schema = '$db_name' AND table_name LIKE '$pattern%'";
+$query=mysqli_query($conn,$sql);
+//$row=mysqli_fetch_array($query);
+while($row=mysqli_fetch_array($query)){
+    //echo $pattern;
+    echo "".$row["0"];
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,5 +23,10 @@
 </head>
 <body>
     <button onclick =" window.location.href='insertBudget.php'">ADD BUDGET</button>
+    <div container>
+    <?php 
+  
+    ?>
+    </div>
 </body>
 </html>
