69ed6ea6b67cff6f011e3da8881989d4e8ce0d93
XDRONIN <akashprakashmattathil@gmail.com>
2024-08-05 20:15:39 +0530

    navbar on budget, name on profile

--- budget.php
+++ budget.php
@@ -2,6 +2,7 @@
 session_start();
 include("connect.php");
 $tbname=$_SESSION['tbname'];
+$name=$_SESSION['name'];
 $pattern = $tbname."_";
 
 
@@ -18,7 +19,20 @@ $query=mysqli_query($conn,$sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Budget</title>
 </head>
+<link
+      href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+ <link rel="stylesheet" href="budget.css">
 <body>
+<div class="navbar">
+<h2 class="logo">Coin<span class="span1">Trail</span></h2>
+      <div class="profile"><?php echo $name?></div>
+</div>
     <button onclick =" window.location.href='insertBudget.php'">ADD BUDGET</button>
     <div container>
     <?php 
