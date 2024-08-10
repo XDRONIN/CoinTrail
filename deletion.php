93b4cbfd6a052f4cca7939d023234af74ed5017a
XDRONIN <akashprakashmattathil@gmail.com>
2024-08-10 13:09:28 +0530

    Deletion feature added to budgets

--- deletion.php
+++ deletion.php
@@ -1,4 +1,16 @@
-<?php 
+<?php
+include('connect.php'); 
 $bTb=$_POST['Btb-input'];
-echo''.$bTb.'';
+//echo''.$bTb.'';
+
+$dltsql="DROP TABLE $bTb";
+$result = mysqli_query($conn,$dltsql);
+if($result){
+   // echo "DELETION SUUCESSFULL";
+   header("Location:http://localhost/MiniProject/budget.php
+    ");
+    exit();
+}
+else{
+    echo "DELETION FAILED";}
 ?>
