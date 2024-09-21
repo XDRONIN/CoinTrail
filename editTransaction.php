d2420e4cbded078b1aefac2086647b8276471ab1
XDRONIN <akashprakashmattathil@gmail.com>
2024-09-21 19:09:37 +0530

    edit input initialization

--- editTransaction.php
+++ editTransaction.php
@@ -29,8 +29,41 @@ $name=$_SESSION['name'];?>
     </div>  
     <div>
     <?php $id=$_POST['id'];
-    echo ''.$id.'';
+    //echo ''.$id.'';
+    $sql="SELECT * FROM $tbname WHERE ID='$id'";
+    $query=mysqli_query($conn,$sql);
+    $result=mysqli_fetch_array($query);
+   //echo $result[2];
+    if($result[1]=='credit'){
     ?>
+    <div class="container">
+    <div class="card">
+      <center><h2>Credit</h2></center>
+    <form method="post" action="updateTransaction.php" >
+    <b>Amount:</b><input type="number" name='amount' value='<?php echo $result[2]?>' class="input"><br>
+   <b> Date:</b> &nbsp; &nbsp;   &nbsp;  <input type="date" name='date' value='<?php echo $result[5]?>' class="input"><br>
+   <center> <input class="btn" type="submit"></center>
+    </form>
+    </div>
+    </div>
+    <?php } ?>
+   <?php if($result[1]=='debit'){
+    ?>
+    <div class="container">
+    
+    <div class="card" style="padding: 50px;">
+      <center><h2>Debit</h2></center>
+    <form method="post" action="updateTransaction.php" >
+    <b>Amount:</b><input type="number" name='amount' value='<?php echo $result[2]?>' class="input"><br>
+    <b>Catogory:</b><input type="text" name='catogory' value='<?php echo $result[3]?>' class="input"><br>
+    <b>Sub-Catogory:</b><input type="text" name='scatogory' value='<?php echo $result[4]?>' class="input"><br>
+    
+   <b> Date:</b> &nbsp;&nbsp;&nbsp;  <input type="date" name='date' value='<?php echo $result[5]?>' class="input"><br>
+   <center> <input class="btn" type="submit"></center>
+    </form>
+    </div>
+    </div>
+    <?php } ?>
     </div>
 </body>
 </html>
